59869810e2bd019a7bc5f6707f08f0b4e6b5cdea
Gabriel Arusiewicz Berta <gabriaru@gmail.com>
2022-12-13 20:36:07 -0300

    sdfsdfsdfsdf

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Natal from './cabeca';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Natal/>
   </React.StrictMode>
 );
 
