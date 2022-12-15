0b1f04f50d8febeaa28c27e3aabf7609b07bad8d
Gabriel Arusiewicz Berta <gabriaru@gmail.com>
2022-12-14 23:15:31 -0300

    remoção de arquivos e adição de imagens

---
+++ src/imagem1.jsx
@@ -0,0 +1,11 @@
+import style from './imagem1.css'
+
+export default function Imagem1 ()
+{
+    return(
+
+        <div className={style.container}>
+            <img src='ArvoreDeNatal.png' />
+        </div>
+    )
+}
