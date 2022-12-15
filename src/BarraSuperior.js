5fb3066d1f59a69540a470bc8a4eac422579fb86
Gabriel Arusiewicz Berta <gabriaru@gmail.com>
2022-12-14 23:17:33 -0300

    imagens adicionadas e arquivos teste removidos

---
+++ src/BarraSuperior.js
@@ -0,0 +1,14 @@
+export default function BarraSuperior()
+{
+    return(
+        <nav>
+            <ul>
+                <li><a href="#home">HOME</a></li>
+                <li><a href="#produtos">PRODUTOS</a></li>
+                <li><a href="#sobre">SOBRE</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+//import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Natal from './cabeca';
+import BarraSuperior from './BarraSuperior';
+import Imagem1 from './imagem1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Natal/>
+
+  <BarraSuperior/>
+  <Imagem1/>
+
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
