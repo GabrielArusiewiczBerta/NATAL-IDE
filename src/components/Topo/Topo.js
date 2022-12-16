4b6f88519343285b2b473c7cb8e0329f1ad6f106
Gabriel Arusiewicz Berta <gabriaru@gmail.com>
2022-12-16 00:01:16 -0300

    imagem arrumada parte inferior do site adicionada inico do texto central e organização melhorada
    Co-authored-by: Carol <CarolMattiello@users.noreply.github.com>
    Co-authored-by: Cauê Scotti <Caue-Scotti@users.noreply.github.com>

---
+++ src/components/Topo/Topo.js
@@ -0,0 +1,16 @@
+import FotoNatal from './ArvoreNatal.png';
+import styles from './Topo.module.css';
+
+function Topo (){
+
+    return (
+
+        <div className={styles.container}>
+            <img className={styles.FotoTopo} src={FotoNatal} alt='Foto Natal'></img>
+            <p className={styles.Degrade}></p>
+            <h2 className={styles.FraseTopo}>Promoção de Natal na DecorHouse!</h2>
+        </div>
+    )
+}
+
+export default Topo
